f1a70bbaab7943a2ba659461d27fa5bad398d113
Rodrigo RB <rodrigo.rb.dev@gmail.com>
2020-12-25 18:05:25 -0300

    feat: add MediaMatch tests.

---
+++ src/components/MediaMatch/MediaMatch.test.tsx
@@ -0,0 +1,46 @@
+import { render, screen } from '@testing-library/react'
+import 'jest-styled-components'
+
+import MediaMatch from './MediaMatch'
+
+describe('<MediaMatch />', () => {
+  let hiddenHeading: Element
+  let desktopHeading: Element
+  let mobileHeading: Element
+
+  beforeEach(() => {
+    render(
+      <>
+        <MediaMatch>
+          <h1 data-testid="hidden">Always Hidden</h1>
+        </MediaMatch>
+        <MediaMatch greaterThan="medium">
+          <h1 data-testid="desktop">Desktop</h1>
+        </MediaMatch>
+        <MediaMatch lessThan="medium">
+          <h1 data-testid="mobile">Mobile</h1>
+        </MediaMatch>
+      </>
+    )
+
+    hiddenHeading = screen.getByTestId('hidden')
+    desktopHeading = screen.getByTestId('desktop')
+    mobileHeading = screen.getByTestId('mobile')
+  })
+
+  it('should be hidden if no media query is passed', () => {
+    expect(hiddenHeading.parentElement).toHaveStyleRule('display', 'none')
+    expect(desktopHeading.parentElement).toHaveStyleRule('display', 'none')
+    expect(mobileHeading.parentElement).toHaveStyleRule('display', 'none')
+  })
+
+  it('should show children based on the passed media query', () => {
+    expect(desktopHeading.parentElement).toHaveStyleRule('display', 'block', {
+      media: '(min-width: 768px)'
+    })
+
+    expect(mobileHeading.parentElement).toHaveStyleRule('display', 'block', {
+      media: '(max-width: 768px)'
+    })
+  })
+})
