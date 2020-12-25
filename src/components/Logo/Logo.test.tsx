a36e23f5442b734c813d9fff6af1e384aab57616
Rodrigo RB <rodrigo.rb.dev@gmail.com>
2020-12-25 15:04:10 -0300

    feat: add tests for Logo component. Add additional jest config. Add theme helper for tests.

---
+++ src/components/Logo/Logo.test.tsx
@@ -0,0 +1,52 @@
+import { screen } from '@testing-library/react'
+import 'jest-styled-components'
+
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Logo from './Logo'
+
+describe('<Logo />', () => {
+  it('should render a white-colored label by default', () => {
+    renderWithTheme(<Logo />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+
+  it('should render a black-colored label when color is passed', () => {
+    renderWithTheme(<Logo color="black" />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
+  })
+
+  it('should render a normal-sized logo when the size is default', () => {
+    renderWithTheme(<Logo />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
+
+  it('should render a bigger-sized logo when the size is passed', () => {
+    renderWithTheme(<Logo size="large" />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
+    })
+  })
+
+  it('should render a bigger-sized logo without the text if hideOnMobile', () => {
+    renderWithTheme(<Logo hideOnMobile />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      {
+        media: '(max-width: 768px)'
+      }
+    )
+  })
+})
