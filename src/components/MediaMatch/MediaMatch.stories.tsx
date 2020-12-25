48993a2543c796c4654b9e2a4e83a16f221b98e5
Rodrigo RB <rodrigo.rb.dev@gmail.com>
2020-12-25 18:05:41 -0300

    feat: add MediaMatch stories.

---
+++ src/components/MediaMatch/MediaMatch.stories.tsx
@@ -0,0 +1,26 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+import MediaMatch, { MediaMatchProps } from './MediaMatch'
+
+export default {
+  title: 'MediaMatch',
+  component: MediaMatch,
+  args: {}
+} as Meta
+
+export const Desktop: Story<MediaMatchProps> = (args) => (
+  <MediaMatch greaterThan="medium" {...args}>
+    Only on Desktop
+  </MediaMatch>
+)
+
+export const Mobile: Story<MediaMatchProps> = (args) => (
+  <MediaMatch lessThan="medium" {...args}>
+    Only on Mobile
+  </MediaMatch>
+)
+Mobile.parameters = {
+  viewport: {
+    defaultViewport: 'mobile1'
+  }
+}
